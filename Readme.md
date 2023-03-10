6d418327ac4d36c6bff6e0105529a73cc903a614
Isaiah Grace <irgkenya4@gmail.com>
2023-03-10 13:48:17 -0500

    Let's try copying the service files, instead of using symlinks.

--- Readme.md
+++ Readme.md
@@ -2,4 +2,4 @@
 This is a collection of simple Python scripts and Systemd timers/services which work together to send daily updates on the tides in Ocean Park Maine to a Telegram chat.
 
 ## Installation
-To install, run the `install.sh` script with root privileges. The script creates symlinks in `/etc/systemd/system` so you can update the installation by updating the files in this repo.
+To install, run the `install.sh` script with root privileges. The script copies service files to `/etc/systemd/system` so you can update the installation by re-running the install script.
--- install.sh
+++ install.sh
@@ -8,15 +8,15 @@ if [[ $(pwd) != "/home/isaiah/OceanParkTidesBot" ]]; then
 fi
 
 if [[ $(whoami) != "root" ]]; then
-	echo "This script creates symlinks to /etc/systemd/system/ and therefore needs to be run as root."
+	echo "This script creates copies files to /etc/systemd/system/ and therefore needs to be run as root."
 	exit 1
 fi
 
-echo "Creating symlinks in /etc/systemd/system for systemd services and timers."
-ln -sf OPTB_daily_noaa_fetch.timer /etc/systemd/system/OPTB_daily_noaa_fetch.timer
-ln -sf OPTB_daily_noaa_fetch.service /etc/systemd/system/OPTB_daily_noaa_fetch.service
-ln -sf OPTB_morning_info.service /etc/systemd/system/OPTB_morning_info.service
-ln -sf OPTB_low_tide_notify.service /etc/systemd/system/OPTB_low_tide_notify.service
+echo "Copying service files to /etc/systemd/system."
+cp -vf OPTB_daily_noaa_fetch.timer /etc/systemd/system/OPTB_daily_noaa_fetch.timer
+cp -vf OPTB_daily_noaa_fetch.service /etc/systemd/system/OPTB_daily_noaa_fetch.service
+cp -vf OPTB_morning_info.service /etc/systemd/system/OPTB_morning_info.service
+cp -vf OPTB_low_tide_notify.service /etc/systemd/system/OPTB_low_tide_notify.service
 
 echo "systemctl daemon-reload"
 systemctl daemon-reload
